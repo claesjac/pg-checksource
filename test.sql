74021311997b58a087ecff1d5c1b9896ac071ffd
Claes Jakobsson <claes@surfar.nu>
2012-01-15 17:43:31 +0100

    Added a long list of keywords to the keywords checks. Since we probably want to reuse that list we need some variable substituion of whatever in the config DSL really

--- test.sql
+++ test.sql
@@ -1,3 +1,3 @@
-SELECT COUNT(*), x*y, SUM(CASE WHEN Foo IS NOT NULL THEN 1 ELSE 0 END)
+select COUNT(*), x*y, SUM(CASE WHEN Foo IS NOT NULL THEN 1 ELSE 0 END)
   FROM Bar 
- WHERE Quax = $1 AND Datestamp>=now() -'1 hour'::interval;
+ WHERE Quax = $1 and Datestamp>=now() -'1 hour'::interval;
